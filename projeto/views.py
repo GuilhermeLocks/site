86f693022530f301eadfec6f45eb1cd1a9eda033
Guilherme da Silva Locks <guisilvalocks@gmail.com>
2025-12-15 08:03:29 -0300

    teste html

--- projeto/views.py
+++ projeto/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def ona_view(request):
-    return render(request, 'template/index.html')
+    return render(request, 'template/ona.html')
 
 def index_view(request):
     #return HttpResponse("Padrão Ona 2")
-    return render(request, 'index.html')
+    return render(request, 'ona.html')
 
